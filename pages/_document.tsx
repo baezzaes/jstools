098257e94da7b613c9c19798f77e31d84ec7a4c4
baezzaes <baezzaes@naver.com>
2025-04-04 23:44:16 +0900

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,43 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          {/* Test: Add meta tag */}
-          <meta name="author" content="JSTools Team" />
-          <meta name="description" content="This is a test page for adding extra tags." />
-
-          {/* Google Analytics */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-F8GPFH41SR"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-F8GPFH41SR');
-              `,
-            }}
-          />
-
-          {/* Test: Adding an external script */}
-          <script
-            async
-            src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
-          ></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/N_logo.svg" />
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument
