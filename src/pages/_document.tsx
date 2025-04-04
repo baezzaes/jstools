86588eec5b59591412e2b25db1f314ca44b3bfdc
baezzaes <baezzaes@naver.com>
2025-04-04 18:13:12 +0900

    구글애널리틱스추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,24 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          {/* Google 사이트 소유권 확인 메타 태그 */}
-          <meta name="google-site-verification" content="U4d1DaTsfSAS7FfdfRbatn8szv1ujLZPXeoZZ43mLYI" />
+          {/* Google Analytics 4 */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-F8GPFH41SR`}  // GA4 측정 ID로 변경
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                
+                gtag('config', 'G-F8GPFH41SR');  // GA4 측정 ID로 변경
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
